cfb2c75ee30d4187e5ebfc066901d743a76d27ed
dice-game <darya28.sk@gmail.com>
2026-05-08 15:39:17 +0530

    added 2 pages

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,8 +1,10 @@
 import {Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss',
 })
